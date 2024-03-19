52fdcc04e86e102d9adfc9f96fb2797dc8b55854
Alexander Harring <alexanderharring@gmail.com>
2024-03-19 22:58:28 +0000

    Initial Commit

---
+++ WordWizard/Controllers/HomescreenController.swift
@@ -0,0 +1,27 @@
+//
+//  HomescreenController.swift
+//  WordWizard
+//
+//  Created by Alexander Harring on 12/03/2024.
+//
+
+import UIKit
+
+class HomescreenController: UIViewController {
+    
+    @IBOutlet weak var titleWordWiseText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        titleWordWiseText.layer.shadowColor = UIColor.appLightGreen.cgColor
+//        titleWordWiseText.layer.shadowOpacity = 0.7
+//        titleWordWiseText.layer.shadowRadius = 5
+//        
+//        let shadowSize: CGFloat = 20
+//        let shadowPath = CGPath(ellipseIn: CGRect(x: -shadowSize, y: 60, width: titleWordWiseText.layer.bounds.width, height: 1), transform: nil)
+//        
+//        titleWordWiseText.layer.shadowPath = shadowPath
+    }
+    
+}
